265496e6a2cba5c0df3ffe143968fb3da8d69a45
antoinet <antoine@schoggi.org>
2013-10-02 22:57:51 +0200

    added readystates

---
+++ readystates/readystates.js
@@ -0,0 +1,9 @@
+xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+	var newListElement = document.createElement('li');
+	var text = document.createTextNode('readyState: ' + xhr.readyState + ', HTTP Status: ' + xhr.status);
+	newListElement.appendChild(text);
+	document.getElementById('states').appendChild(newListElement);
+};
+xhr.open('POST', '/echo/xml');
+xhr.send();
