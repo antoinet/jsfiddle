09acf14c418d2d3df87310e2c9b4e02b65790e8c
antoinet <antoine@schoggi.org>
2013-10-09 22:33:52 +0200

    added button with onlick

--- 08-flickr-jsonp/demo.js
+++ 08-flickr-jsonp/demo.js
@@ -2,7 +2,9 @@ function jsonFlickrApi(json) {
     document.getElementById('img1').src = json.sizes.size[3].source;
 }
 
-var script_element = document.createElement('script');
-script_element.src = 'http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&photo_id=8649522188&api_key=286a91389f0175f572466c707ba85df9&format=json';
-document.getElementsByTagName('head')[0].appendChild(script_element);
+function button_click() {
+    var script_element = document.createElement('script');
+    script_element.src = 'http://api.flickr.com/services/rest/?method=flickr.photos.getSizes&photo_id=8649522188&api_key=286a91389f0175f572466c707ba85df9&format=json';
+    document.getElementsByTagName('head')[0].appendChild(script_element);
+}
 
