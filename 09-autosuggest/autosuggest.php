378f9b404cea391010acd497413fc0936abd9b10
Antoine Neuenschwander <antoine@schoggi.org>
2014-08-18 18:12:04 +0200

    added callback parameter to autosuggest.php

--- 09-autosuggest/autosuggest.php
+++ 09-autosuggest/autosuggest.php
@@ -1,13 +1,15 @@
 <?php
-	
 if (isset($_REQUEST['query'])) {
-	$query = $_REQUEST['query'];
-	
-	$countries = file("countries.txt");
-	
-	$result = array_values(preg_grep("/$query/i", $countries));
-
-	header('Content-Type', 'application/json');
+$query = $_REQUEST['query'];
+$callback = $_REQUEST['callback'];
+$countries = file("countries.txt");
+$result = array_map('trim', array_values(preg_grep("/^$query/i", $countries)));
+header('Content-Type', 'application/json');
+if ($callback) {
+	echo $callback.'('.json_encode($result).')';
+} else {
 	echo json_encode($result);
 }
+}
 ?>
+
